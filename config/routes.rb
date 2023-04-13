10244b5e5f0b4097e02a7d4a086d6ef4f6d9f926
hinata000 <hinata0125125@icloud.com>
2023-04-13 17:45:18 +0900

    animationコントローラーにアクションを追加しルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   root 'homes#index'
+
+  resources :animations, only: [:index, :show,] do
+    resources :favorites, only: [:create, :destroy]
+    resources :reviews, only: [:index, :new, :create]
+    collection do
+      get 'search_results'
+    end
+  end
 end
