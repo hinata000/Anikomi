1ced2d69ed62b7af398c41f7b1c1456b001d732d
hinata000 <hinata0125125@icloud.com>
2023-05-06 13:42:14 +0900

    headerの検索バーをransackに対応

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :set_search
+
+  def set_search
+    @q = Animation.ransack(params[:q])
+    @animations = @q.result(distinct: true).order(created_at: :desc).page(params[:page]).per(16)
+  end
 end
